84baf43cde90d7189a9949495cfd0c51483f11f5
damianolagana <damianolagana@gmail.com>
2023-05-30 16:11:19 +0200

    first commit

---
+++ vite-project/src/App.vue
@@ -0,0 +1,41 @@
+<script >
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+
+}
+</script >
+
+<template>
+  <header>
+    <AppHeader />
+  </header>
+  <main>
+    <AppMain />
+  </main>
+  <footer>
+    <AppFooter />
+  </footer>
+</template>
+
+
+</script>
+
+
+<style scoped></style>
